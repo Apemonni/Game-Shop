511f614213c45e3d74b40b473bc542a1256392ba
aapolinjama <aapo.linjama@gmail.com>
2020-02-04 13:20:34 +0200

    Adding load and save functions to views.py

--- gameShop/PlayingGame/views.py
+++ gameShop/PlayingGame/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+def load(request, player_id, game_id):
+    return HttpResponse("Loading the game")
+
+def save(request, player_id, game_id):
+    return HttpResponse("Saving the game")
