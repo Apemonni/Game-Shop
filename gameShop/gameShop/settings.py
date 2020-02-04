e2cefcafebffb0badfd24e2850271792e0bf8dc2
aapolinjama <aapo.linjama@gmail.com>
2020-02-04 13:37:48 +0200

    Adding PlayingGame to installed apps.

--- gameShop/gameShop/settings.py
+++ gameShop/gameShop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'PlayingGame.apps.PlayinggameConfig',
     'frontpage.apps.FrontpageConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
