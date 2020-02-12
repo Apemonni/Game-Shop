08c0fb575616cbd65e85d4b395ed8de1c32260c4
Oskari Mäkinen <jaakko.makinen@aalto.fi>
2020-02-12 13:07:13 +0200

    Refactor
    - move separate script from html
    - use base template in play.html

--- gameShop/play_game/static/play_game/communicate.js
+++ gameShop/play_game/static/play_game/communicate.js
@@ -1,33 +1,48 @@
 /* global $ */
-  $(document).ready(function() {
-    'use strict';
+$(document).ready(function() {
+  'use strict';
+  var game_iframe = $("#game_iframe")[0]
 
-    $(window).on('message', function(evt) {
-      //Note that messages from all origins are accepted
+  $(window).on('message', function(evt) {
+    //Note that messages from all origins are accepted
 
-      //Get data from sent message
-      var data = JSON.parse(JSON.stringify(evt.originalEvent.data));
-      //Create a new list item based on the data
-
-      //alert(data.gameState.score)
-      if (data.messageType == "SAVE"){
-        $.ajax({
-          url: 'gameShop/play_game/views.py',
-          data: {
-            'save_message': data
-          },
-          success: function () {
-              alert("A user with this username already exists.");
-              var newItem = '\n\t<li>' + (data.score) + '</li>';
-              //Add the item to the beginning of the actions list
-              $('#actions').prepend(newItem);
-          }
-
-        });
-      }
-
-      var newItem = '\n\t<li>' + (data.gameState.score) + '</li>';
-      //Add the item to the beginning of the actions list
-      $('#actions').prepend(newItem);
+    //Get data from sent message
+    var data = evt.originalEvent.data;
+    if (data.messageType == "SAVE"){
+      $.ajax({
+        url: game_iframe.getAttribute("save_url"),
+        data: {
+          'game_state': JSON.stringify(data.gameState)
+        },
+        dataType: 'json'
     });
+    }
+    else if (data.messageType == "LOAD_REQUEST"){
+      $.ajax({
+        url: game_iframe.getAttribute("load_url"),
+        dataType: 'json',
+        success: function (data) {
+          // A responser with LOAD and game state OR ERROR and info message is returned after load request
+          if(data.messageType == "LOAD") {
+            data.gameState = JSON.parse(data.gameState) // Convert gameState back to json format
+          }
+          game_iframe.contentWindow.postMessage(data, '*')
+        }
+      })
+    }
+    else if(data.messageType == "SCORE"){
+      $.ajax({
+        url: game_iframe.getAttribute("score_url"),
+        data: {
+          'score': data.score
+        },
+        dataType: 'json'
+      })
+    }
+    else if(data.messageType == "SETTING") {
+      //alert("received setting call: " + data.options.width + ", " + data.options.height)
+      game_iframe.setAttribute("width", data.options.width)
+      game_iframe.setAttribute("height", data.options.height)
+    }
   });
+});
