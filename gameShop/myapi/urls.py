8bdc063ef394124cf85420ad06fae02851441bff
aapolinjama <aapo.linjama@gmail.com>
2020-02-13 22:36:13 +0200

    Changing the namespacing of the urls in API.

--- gameShop/myapi/urls.py
+++ gameShop/myapi/urls.py
@@ -3,10 +3,10 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'gamedatas', views.GameDataViewSet)
-router.register(r'games', views.GameViewSet)
-router.register(r'profiles', views.ProfileViewSet)
-router.register(r'users', views.UserViewSet)
+router.register(r'Gamedatas', views.GameDataViewSet)
+router.register(r'Games', views.GameViewSet)
+router.register(r'Profiles', views.ProfileViewSet)
+router.register(r'Users', views.UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
